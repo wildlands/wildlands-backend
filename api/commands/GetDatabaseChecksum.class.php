e9e002eb52045e6c4b70a6ce7c77987ae45144cb
Maximilian Wiedemann <max@majoware.de>
2015-05-19 10:57:46 +0200

    GetDatabaseChecksum command now includes 'level' table.

--- api/commands/GetDatabaseChecksum.class.php
+++ api/commands/GetDatabaseChecksum.class.php
@@ -18,7 +18,7 @@ class GetDatabaseChecksum extends Command
 
     public function execute($parameter)
     {
-        $query = "CHECKSUM TABLE answer, pinpoint, question, page;";
+        $query = "CHECKSUM TABLE answer, pinpoint, question, page, level;";
         $result = query($query);
 
         $database = new Database();
