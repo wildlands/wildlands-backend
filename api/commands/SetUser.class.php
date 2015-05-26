f8b22b8af817393d34d31ba664457d1483f6a834
maaike1193 <maaikeveurink@hotmail.com>
2015-05-26 15:58:54 +0200

    wachtwoord check geouwehoer

--- api/commands/SetUser.class.php
+++ api/commands/SetUser.class.php
@@ -22,27 +22,43 @@ class SetUser extends Command
             $query = "UPDATE user SET Screenname = '$user->name', Email = '$user->email'";
             if (isset($user->password))
             {
-                $hashpass = password_hash($user->password, PASSWORD_DEFAULT);
-                $query .= ", Password = '$hashpass'";
+                $querypass = query("SELECT Password FROM user WHERE UserID = '$user->id'");
+                $fetch = $querypass->fetchassoc();
+                $password = $user->oldpassword;
+                
+                if(password_verify($password, $fetch['Password']))
+                {
+                    $querypass = query("SELECT Password FROM user WHERE UserID = '$user->id'");
+                    $fetch = $querypass->fetchassoc();
+                    $password = $user->oldpassword;
+                
+                    $hashpass = password_hash($user->password, PASSWORD_DEFAULT);
+                    $query .= ", Password = '$hashpass'";
+                } 
+                else
+                {
+                    errorMessage("Oude wachtwoord komt niet overeen!");
+                }
+
+                $query .= " WHERE UserID = '$user->id';";
+                $successMessage = "Gebruiker is aangepast.";
             }
-            $query .= " WHERE UserID = '$user->id';";
-            $successMessage = "Gebruiker is aangepast.";
-        }
-        else
-        {
-            $hashpass = password_hash($user->password, PASSWORD_DEFAULT);
-            $query = "INSERT INTO user (Screenname, Email, Password) VALUES ('$user->name', '$user->email', '$hashpass');";
-            $successMessage = "Gebruiker is aangemaakt.";
-        }
+            else
+            {
+                $hashpass = password_hash($user->password, PASSWORD_DEFAULT);
+                $query = "INSERT INTO user (Screenname, Email, Password) VALUES ('$user->name', '$user->email', '$hashpass');";
+                $successMessage = "Gebruiker is aangemaakt.";
+            }
 
-        $result = query($query);
+            $result = query($query);
 
-        if (!$result)
-        {
-            $this->errorMessage("Er is iets fout gegaan.");
+            if (!$result)
+            {
+                $this->errorMessage("Er is iets fout gegaan.");
+            }
+
+            successMessage($successMessage);
         }
-
-        successMessage($successMessage);
     }
 }
 
